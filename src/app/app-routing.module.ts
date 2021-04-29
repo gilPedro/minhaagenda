16eb7d3f8115e4841f8d3d9963f1d38763d35c98
gilPedro <vando.pedro@live.com>
2021-04-28 21:30:34 -0300

    with '#' will be ignored, and an empty message aborts the commit.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,34 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'login',
+    loadChildren: () => import('./paginas/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'registro',
+    loadChildren: () => import('./paginas/registro/registro.module').then( m => m.RegistroPageModule)
+  },
+  {
+    path: 'contato',
+    loadChildren: () => import('./paginas/contato/contato.module').then( m => m.ContatoPageModule)
+  },
+  {
+    path: 'edita-contato',
+    loadChildren: () => import('./paginas/edita-contato/edita-contato.module').then( m => m.EditaContatoPageModule)
+  },
+  {
+    path: 'tarefa',
+    loadChildren: () => import('./paginas/tarefa/tarefa.module').then( m => m.TarefaPageModule)
+  },
+  {
+    path: 'edita-tarefa',
+    loadChildren: () => import('./paginas/edita-tarefa/edita-tarefa.module').then( m => m.EditaTarefaPageModule)
+  },
+  {
+    path: 'inicio',
+    loadChildren: () => import('./paginas/inicio/inicio.module').then( m => m.InicioPageModule)
+  },
 ];
 
 @NgModule({
